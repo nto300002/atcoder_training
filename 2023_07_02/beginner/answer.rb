24cfeebeef1fbc78f88dff21e2f64cbbf79f9e6d
nto300002 <samonkntd@gmail.com>
2023-06-27 23:52:33 +0900

    2023_07_02

--- 2023_07_02/beginner/answer.rb
+++ 2023_07_02/beginner/answer.rb
@@ -1,23 +1,23 @@
-#問題1： 解答
+#問題1： 
 n = gets.to_i #gets.to_i 好きな数字に置き換え
 puts n.even? ? "Even" : "Odd"
 
-#問題2： 解答
+#問題2： 
 n = gets.to_i
 puts (1..n).sum
 
-#問題3： 解答
+#問題3： 
 a = 1
 b = 2
 arr = [a, b]
 arr.map(&:to_i)
 puts arr.max
 
-#問題4： 解答
+#問題4： 
 n = gets.to_i
 puts n % 3 == 0 ? "YES" : "NO"
 
-#問題5： 解答
+#問題5： 
 n = gets.to_i
 if n > 10
   puts "GREATER"
@@ -27,7 +27,7 @@ else
   puts "EQUAL"
 end
 
-#問題6： 解答
+#問題6： 
 a = 3
 b = 1
 c = 11
@@ -35,19 +35,19 @@ arr = [a, b, c]
 arr.map(&:to_i)
 puts arr.min
 
-#問題7： 解答
+#問題7： 
 n = gets.to_i
 puts n.to_s.length
 
-#問題8： 解答
+#問題8： 
 n = gets.to_i
 puts Math.sqrt(n).to_i
 
-#問題9： 解答
+#問題9： 
 require 'prime'
 n = gets.to_i
 puts Prime.prime?(n) ? "YES" : "NO"
 
-#問題10： 整数nが与えられるので、その数が負の数であるかを判定してください。負の数ならNEGATIVE、そうでないならPOSITIVEを出力してください。
+#問題10： 
 n = gets.to_i
 puts n < 0 ? "NEGATIVE" : "POSITIVE"
